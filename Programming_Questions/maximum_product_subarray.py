ba84fe4db8c39b221f93e322683c113403d02769
bgeetika <geetikagarg07@gmail.com>
2015-09-14 20:49:44 -0700

    update maximum_product_subarray

--- Programming_Questions/maximum_product_subarray.py
+++ Programming_Questions/maximum_product_subarray.py
@@ -1,25 +1,20 @@
 class Solution(object):
     def maxProduct(self, nums):
-        res = []
-        res.append(nums[0])
+        max_product = nums[0]
+        min_product = nums[0]
+        res_last = nums[0]
+        min_last = nums[0]
         
-        min_arr = []
-        min_arr.append(nums[0])
         
         for i in range(1, len(nums)):
-            res_last = res[-1]
-            min_last =  min_arr[-1]
-            res.append(max(nums[i], res_last*nums[i], min_last*nums[i]))
-            min_arr.append( min( nums[i], min_last*nums[i], res_last*nums[i]))
-        max_pro = float("-infinity")
-        
-        print min_arr
-        for i in range(0, len(res)):
-            max_pro = max(max_pro, res[i])
-            
-        return max_pro
+            res_last_val = res_last
+            min_last_val =  min_last
+            res_last = (max(nums[i], res_last_val*nums[i], min_last_val*nums[i]))
+            min_last = (min(nums[i], res_last_val*nums[i], min_last_val*nums[i]))
+            max_product = max(max_product, res_last)
+            min_product = min(min_product, min_last)
+            print max_product, min_product
+        return max(max_product, min_product)
         """
         :type nums: List[int]
         :rtype: int
-        """
-        
